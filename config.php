f0b488d972a3ebfcc0d71ddbe034f1b190f9b1ab
bokele wakiza franck <bwakiza@gmail.com>
2024-03-06 10:05:55 +0200

    database connection

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+require 'Database.php';
+
+
+
+return [
+    'database' => [
+        "host" => "localhost",
+        "port" => 3306,
+        "dbname" => "simple_php",
+        "charset" => "utf8mb4"
+    ]
+];
--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
 require 'functions.php';
-
+require 'Database.php';
 require 'router.php';
+
+
+   $config = require('config.php');
+
+        $db= new Database($config['database']);
+
+
+foreach ($posts as $post) {
+    echo "<li>{$post['title']}</li>";
+}
